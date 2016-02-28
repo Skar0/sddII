e5f7c362b2be6c87d378507070f95dd072b60d5f
Clément Tamines <clement.tamines@gmail.com>
2016-02-28 23:36:23 +0100

    Started implementing painter's algorithm

--- src/be/ac/umons/gui/mainPanel.java
+++ src/be/ac/umons/gui/mainPanel.java
@@ -1,7 +1,42 @@
 package be.ac.umons.gui;
 
+import be.ac.umons.bsp.BSPNode;
+import be.ac.umons.bsp.Segment;
+
+import javax.swing.*;
+import java.util.List;
+
 /**
  * Created by clement on 2/28/16.
  */
-public class mainPanel {
+public class mainPanel extends JPanel {
+
+    public void paintersAlgorithm(BSPNode root, double[] pov) {
+        if (root.isLeaf()) {
+           drawSegments(root.getSegmentsInLine());
+        }
+        else if (getPovPosition(pov).isInfinite()) {
+            paintersAlgorithm(root.getLeftSon(), pov);
+            drawSegments(root.getSegmentsInLine());
+            paintersAlgorithm(root.getRightSon(), pov);
+        }
+        else if (getPovPosition(pov).isNaN()) {
+            paintersAlgorithm(root.getRightSon(), pov);
+            drawSegments(root.getSegmentsInLine());
+            paintersAlgorithm(root.getLeftSon(), pov);
+        }
+        else {
+            paintersAlgorithm(root.getRightSon(), pov);
+            paintersAlgorithm(root.getLeftSon(), pov);
+        }
+    }
+
+    private Double getPovPosition(double[] pov) {
+        return 5.0;
+    }
+
+    public void drawSegments(List<Segment> segments) {
+
+    }
+
 }
