e8fc50644ae02c9fb55f08f590c377a97765f521
Clément Tamines <clement.tamines@gmail.com>
2016-03-17 21:15:57 +0100

    Added Javadoc and comments

--- src/be/ac/umons/bsp/RandomHeuristic.java
+++ src/be/ac/umons/bsp/RandomHeuristic.java
@@ -4,17 +4,26 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by mr_robot on 04-03-16.
+ * This heuristic is similar to the in-order heuristic but it shuffles the list of segments first.
+ * @author Clément Tamines
  */
 public class RandomHeuristic implements Heuristic {
 
-    @Override
+
+    /**
+     * Creates a BSP tree using the random heuristic
+     * @param segmentList List of segments to make a BSP tree out of
+     * @return The BSPNode that is the root of the created tree.
+     */
     public BSPNode createTree(List<Segment> segmentList) {
+
+        //The random heuristic is just the in-order heuristic applied to a shuffled list of segments.
         InOrderHeuristic inOrder = new InOrderHeuristic();
         Collections.shuffle(segmentList);
         return inOrder.createTree(segmentList);
     }
 
+    //toString is overriden to be used in the gui comboBoxes
     @Override public String toString() {
         return "Random";
     }
