526c6548d47b2e2bef8ff4e3df9ea46bb87222ca
Clément Tamines <clement.tamines@gmail.com>
2016-04-13 15:11:09 +0200

    Added javadoc

--- src/be/ac/umons/gui/Pov.java
+++ src/be/ac/umons/gui/Pov.java
@@ -3,14 +3,23 @@ package be.ac.umons.gui;
 import java.awt.geom.Line2D;
 
 /**
- * Created by mr_robot on 3/3/16.
+ * This class represents a point of view
+ * @author Clément Tamines
  */
 public class Pov {
+    /** The director vector of the point of view (line between the two lines of the pov) **/
     private Line2D directorVector;
+
+    /** The line on which the scene will be projected **/
     private Line2D projectionLine;
+
+    /** The position of the point of view **/
     private double[] position = new double[2];
-    private double angle;
+
+    /** The first line defining the pov **/
     private Line2D line1 = null;
+
+    /** The second line defining the pov **/
     private Line2D line2 = null;
 
     public Pov() {}
@@ -18,17 +27,10 @@ public class Pov {
     public Pov(Line2D line1, Line2D line2) {
         this.line1 = line1;
         this.line2 = line2;
-        this.angle = this.computeAngle(line1,line2);
         this.position[0] = line1.getX1();
         this.position[1] = line1.getY1();
         this.projectionLine = new Line2D.Double(line1.getX2(),line1.getY2(),line2.getX2(),line2.getY2());
         this.directorVector = new Line2D.Double(line1.getX1(),line1.getY1(),(line2.getX2()+line1.getX2())/2D,(line2.getY2()+line1.getY2())/2D);
-    }
-
-    public double computeAngle(Line2D line1, Line2D line2) {
-        double angle1 = Math.atan2(line1.getY1() - line1.getY2(),line1.getX1() - line1.getX2());
-        double angle2 = Math.atan2(line2.getY1() - line2.getY2(), line2.getX1() - line2.getX2());
-        return angle1-angle2;
     }
 
     public double[] getPosition() {
@@ -37,14 +39,6 @@ public class Pov {
 
     public void setPosition(double[] position) {
         this.position = position;
-    }
-
-    public double getAngle() {
-        return angle;
-    }
-
-    public void setAngle(double angle) {
-        this.angle = angle;
     }
 
     public Line2D getLine1() {
